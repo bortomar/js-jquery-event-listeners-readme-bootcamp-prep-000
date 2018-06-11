5859ffc33f099df8cbde369104762d1353db74d9
Mario Bortolotti <bortomar@email.cz>
2018-06-11 18:09:28 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 
 function pressIt() {
   $('#typing').on('keydown', function(e) {
-    if (e.key.toUpperCase() == 'G')
+    if (e && e.key.toUpperCase() == 'G')
       alert('G pressed');
   });
 }
